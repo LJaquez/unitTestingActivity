36f85c85199d6d228d5efa2a48c77e3e9bc0c919
Leticia Jaquez <letty.jaquez@gmail.com>
2022-04-27 09:06:22 -0400

    updated test file

--- greetings.test.js
+++ greetings.test.js
@@ -1,3 +1,28 @@
-test ('Default greeting', () =>{
-    expect(Greet()).toBe('Hello there!');
-})
+var utils = require('course-utilities');
+salutations = utils.load('./greetings.js','greetings')
+
+describe('GREETINGS',() =>{
+    
+    test('function greet() exists', function(){
+        instanceof('should be called', function(){
+            expect(greet('Josephine')).toBeCalled();
+        });
+    });
+
+    test ('Default greeting', () =>{
+        expect(greet()).toBe('Hello there!')
+    });
+
+    test('one name first letter capital', () =>{
+        expect(greet('Josephine').toBe('Hello, Josephine'))
+    });
+
+    test ('All Caps', ()=>{
+        expect(greet('YAN').toBe('HELLO YAN!'))
+    });
+
+    test ('Array', ()=>{
+        expect(greet(arr).toBe('Hello, Yanely, Angleo, Tavi'))
+    });
+
+});
